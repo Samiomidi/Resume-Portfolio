23177cc21560b4f85fd11817045b600591766d88
Sami Omidi <samiomidi@hotmail.com>
2023-04-15 07:13:09 +0430

    skills updated

--- src/Components/Mini/Skill.js
+++ src/Components/Mini/Skill.js
@@ -1,8 +1,62 @@
 import React from "react";
-import { FaJsSquare, FaReact, FaGitSquare } from "react-icons/fa";
-import { SiNextdotjs, SiMui } from "react-icons/si";
-// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FaJsSquare, FaReact, FaGitSquare, FaHtml5 } from "react-icons/fa";
+import {
+  SiNextdotjs,
+  SiMui,
+  SiCss3,
+  SiRedux,
+  SiPwa,
+  SiWordpress,
+} from "react-icons/si";
+
 const icons = {
+  html: (
+    <FaHtml5
+      style={{
+        color: "#e34c26",
+        height: "25",
+        width: "25",
+        backgroundColor: "#fff",
+      }}
+    />
+  ),
+  css: (
+    <SiCss3
+      style={{
+        color: "#264de4",
+        height: "25",
+        width: "25",
+        backgroundColor: "#fff",
+      }}
+    />
+  ),
+  redux: (
+    <SiRedux
+      style={{
+        color: "#764ABC",
+        height: "25",
+        width: "25",
+      }}
+    />
+  ),
+  pwa: (
+    <SiPwa
+      style={{
+        color: "#fff",
+        height: "25",
+        width: "25",
+      }}
+    />
+  ),
+  wordpress: (
+    <SiWordpress
+      style={{
+        color: "#00749C",
+        height: "25",
+        width: "25",
+      }}
+    />
+  ),
   js: (
     <FaJsSquare
       style={{
